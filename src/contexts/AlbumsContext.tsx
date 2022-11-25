1df68028a6d04b1b91c23a1edfc5bd5a7ebcb8ac
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-11-25 20:57:08 -0300

    feat: created album page

--- src/contexts/AlbumsContext.tsx
+++ src/contexts/AlbumsContext.tsx
@@ -12,6 +12,15 @@ type Album = {
   trackCount: number
 }
 
+type Music = {
+  artistName: string
+  collectionName: string
+  kind?: 'song'
+  trackName: string
+  trackId: string
+  previewUrl: string
+}
+
 type AlbumProviderProps = {
   children: React.ReactNode
 }
@@ -19,12 +28,15 @@ type AlbumProviderProps = {
 type AlbumContextData = {
   albums: Album[]
   fetchAlbums: (artist: string) => Promise<void>
+  getCurrentAlbumMusics: (albumId: string) => Promise<void>
+  currentAlbumMusics: Music[]
 }
 
 const AlbumsContext = createContext({} as AlbumContextData)
 
 export function AlbumsProvider({ children }: AlbumProviderProps) {
   const [albums, setAlbums] = useState<Album[]>([])
+  const [currentAlbumMusics, setCurrentAlbumMusics] = useState<Music[]>([])
 
   const fetchAlbums = async (artist: string) => {
     const artistNameURL = encodeURI(artist).replaceAll('%20', '+')
@@ -34,8 +46,19 @@ export function AlbumsProvider({ children }: AlbumProviderProps) {
     setAlbums(data.results)
   }
 
+  const getCurrentAlbumMusics = async (albumId: string) => {
+    const musicAPI = `https://itunes.apple.com/lookup?id=${albumId}&entity=song`
+    const { data } = await httpRequest.get<{ results: Music[] }>(musicAPI)
+
+    const musics = data.results.filter((music) => music.kind)
+
+    setCurrentAlbumMusics(musics)
+  }
+
   return (
-    <AlbumsContext.Provider value={{ albums, fetchAlbums }}>
+    <AlbumsContext.Provider
+      value={{ albums, fetchAlbums, getCurrentAlbumMusics, currentAlbumMusics }}
+    >
       {children}
     </AlbumsContext.Provider>
   )
