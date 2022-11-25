3e8dda1c15b9b4f3f07c7fa54973a7d4432c18d1
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-11-25 16:48:32 -0300

    feat: implemented fetching de albums on home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,30 @@
-import { Button, Flex, Icon, Input, Stack } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Icon,
+  Image,
+  Input,
+  SimpleGrid,
+  Stack,
+  Text
+} from '@chakra-ui/react'
 import { Header } from 'components/Header'
+import { useAlbums } from 'contexts/AlbumsContext'
+import { useState } from 'react'
 import { RiSearchLine } from 'react-icons/ri'
 
 export function Home() {
+  const [artist, setArtist] = useState('')
+
+  const { albums, fetchAlbums } = useAlbums()
+
+  const handleSearch = async () => {
+    await fetchAlbums(artist)
+    setArtist('')
+  }
+
   return (
     <Flex w="100vw" flexDir={'column'}>
       <Header />
@@ -22,13 +44,64 @@ export function Home() {
             px="4"
             mr="4"
             variant={'unstyled'}
+            value={artist}
+            onChange={({ target }) => setArtist(target.value)}
           />
           <Icon as={RiSearchLine} fontSize="20" />
         </Flex>
-        <Button type="button" h="58px" w="150px" colorScheme="linkedin">
+        <Button
+          type="button"
+          h="58px"
+          w="150px"
+          colorScheme="linkedin"
+          onClick={handleSearch}
+        >
           Search
         </Button>
       </Stack>
+
+      {albums.length > 0 && (
+        <Flex w="inherit" mt={30} p={20} flexDir="column">
+          <Heading
+            as="h2"
+            color="gray.500"
+            fontWeight={'light'}
+            fontStyle="italic"
+          >
+            Resultados de álbuns de Artista X:
+          </Heading>
+          <SimpleGrid minChildWidth="240px" gap={4}>
+            {albums.map((album, i) => (
+              <Box
+                key={i}
+                w="240px"
+                borderRadius={'md'}
+                mt={10}
+                boxShadow="2xl"
+              >
+                <Stack spacing={4} textAlign="center">
+                  <Image
+                    src={album.artworkUrl100}
+                    w="100%"
+                    borderTopRadius="md"
+                  />
+                  <Text px={4} fontWeight="bold">
+                    {album.collectionName}
+                  </Text>
+                  <Text
+                    px={4}
+                    color="gray.500"
+                    fontWeight={'medium'}
+                    fontStyle="italic"
+                  >
+                    {album.artistName}
+                  </Text>
+                </Stack>
+              </Box>
+            ))}
+          </SimpleGrid>
+        </Flex>
+      )}
     </Flex>
   )
 }
